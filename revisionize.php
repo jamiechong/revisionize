72c273f9a7c53922f2a6ce4d5fdfe6c82f412886
Alex <self@pirsig.net>
2018-02-09 10:49:44 +0100

    Update revisionize.php
    fix syntax error

--- revisionize.php
+++ revisionize.php
@@ -202,7 +202,7 @@ function copy_post($post, $to=null, $parent_id=null, $status='draft') {
     $new_id = $to->ID;
 
     // maintain original date. Fixes scheduled revisions overwriting the date. see issue #9
-    if (apply_filters('revisionize_preserve_post_date', true, $to->ID) === true)) {
+    if (apply_filters('revisionize_preserve_post_date', true, $to->ID) === true) {
       $data['post_date'] = $to->post_date;
       $data['post_date_gmt'] = get_gmt_from_date($post->post_date);
     }
